ce5558d70d1b1dd4c58977e4dbcfb70478dcf91c
sivapriya256 <sivapriyaece256@gmail.com>
2025-01-06 10:53:08 +0530

    Banner Section

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(".slick-slider").slick({
+    slidesToShow: 3,
+    infinite:false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+      // dots: false, Boolean
+     // arrows: false, Boolean
+   });
+ 
+ 
